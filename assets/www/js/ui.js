c985f9f86aa312f934d3265130e6dc1df9b167c2
Shawn Jansepar <shawn@mobify.com>
2013-03-23 17:14:12 -0700

    Adding forgotten assets

---
+++ assets/www/js/ui.js
@@ -0,0 +1,66 @@
+
+// UI
+
+(function(){
+    
+    // SETUP
+    var POINTS = 1288;
+    var BATTERY = 100;
+
+    var drawPoints = function(){
+        $(".total-points").text(POINTS);
+    }
+
+    var drawBattery = function(batteryLevel){
+        BATTERY = batteryLevel;
+        var batterySrc = "img/battery";
+        if (BATTERY > 50) {
+            batterySrc += "100.png";
+        }
+        else if (BATTERY > 5) {
+            batterySrc += "50.png";
+        }
+        else if (BATTERY == 0) {
+            batterySrc += "0.png";
+        }
+        $(".battery img").attr("src", batterySrc)
+    }
+
+    var setCritterAnimation = function(imgSrc){
+        $(".critter-animation img").attr("src", imgSrc)
+    }
+
+    drawPoints();
+
+    // bind to events
+    $(window).on("batterylow", function(event){
+        setCritterAnimation("img/dance3.gif");
+        drawBattery(10);
+
+    });
+
+    $(window).on("batterycritical", function(event){
+        setCritterAnimation("img/creature-ghost.png");
+        drawBattery(0);
+    });
+
+    // toggle critter panel
+    var $critterPanels = $(".critter-animation, .critter-status");
+    $critterPanels.on("click", function(){
+        $critterPanels.toggleClass("hidden");
+    });
+
+    // toggle menu buttons
+    $(".button.trophies").on("click", function(){
+        $critterPanels.addClass("hidden");
+        $(".trophies-panel").removeClass("hidden");
+    });
+
+    // toggle menu buttons
+    $(".button.critter").on("click", function(){
+        $(".critter-animation").removeClass("hidden");
+        $(".trophies-panel").addClass("hidden");
+    })
+
+
+})();
